58d29cbdeb7b58440d325a726145ea494b89d6c3
saleh mansour <saleh.m1998@hotmail.com>
2015-05-04 15:26:31 +0200

    maybe now pls

--- test_facebook.php
+++ test_facebook.php
@@ -41,7 +41,7 @@ require __DIR__ . '/Facebook/autoload.php';
   try{
     session_start();
     $app_id = '1467879120170390';
-    $app_secret = 'a4eaf9a72fe0335353838672ae7690bf';
+    $app_secret = '8c60b7151c708c98fe47bdf10ef1b4b6';
     $redirect_url = 'http://nd-nextdoor2.rhcloud.com/test_facebook.php';
 
     FacebookSession::setDefaultApplication($app_id, $app_secret);
