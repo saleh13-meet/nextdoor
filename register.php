553e6076c564e313e56d4adb72e16c7432175c51
saleh mansour <saleh.m1998@hotmail.com>
2015-05-04 17:48:29 +0200

    asdasd

--- register.php
+++ register.php
@@ -164,6 +164,7 @@ require __DIR__ . '/Facebook/autoload.php';
       echo "<a href='" . $helper->getLoginUrl(array('email', 'user_about_me')) . "'>Login With FaceBook</a>";
       $string = "saleh mansour";
       $string2 = explode(" ", $string);
+      echo $string2;
     }
   }
   catch(FacebookRequestException $e){
