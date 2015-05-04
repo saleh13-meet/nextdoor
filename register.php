a0d61ed795fd41985f15b278ab8946fe5462fbf2
saleh mansour <saleh.m1998@hotmail.com>
2015-05-04 17:45:14 +0200

    asdasd

--- register.php
+++ register.php
@@ -74,6 +74,8 @@ require __DIR__ . '/Facebook/autoload.php';
       $email = $graph->getEmail();
       $name = $graph->getname();
 
+      $names = explode(" ", $name)
+
       echo "details<br>";
 
       // getting user image
@@ -83,8 +85,80 @@ require __DIR__ . '/Facebook/autoload.php';
       // Display Details.
       echo "stuff<br>";
       echo "Hello $name <br>";
+      echo "firstname: ". $names[0]. "<br>";
+      echo "lastname: ". $names[1]. "<br>";
       echo "Email: $email <br>";
       echo "Your Facebook ID: $id <br>";
+
+      echo '<html>
+	<head>
+		<title>NextDoor</title>
+		<link rel="stylesheet" type="text/css" href="css/register.css">
+		<script type="text/javascript" src="java/jquery-1.11.2.js"></script>
+		<script type="text/javascript" src="java/jquery-ui.js"></script>
+		<script type="text/javascript" src="java/register.js"></script>
+	</head>
+	<body>
+		<header>
+			<div id="header">
+				<a href="index.php"><img src="images/Logo_2.jpg" width="293" height="63"></a>
+			</div>
+		</header>
+		<div id="register-wrapper">
+	        <form method="post" id="register">
+
+	        	<div class="alert-box error numbers"><span>error: </span>Your name must contain letters!</div>
+				<div class="alert-box error wrong"><span>error: </span>Please fill in all the fields!</div>
+				<div class="alert-box error password"><span>error: </span>Password doesnt match!</div>
+				<div class="alert-box error username"><span>error: </span>Username already exist!</div>
+				<div class="alert-box error email"><span>error: </span>Email already exist!</div>
+				<div class="alert-box success thanks"><span>success: </span>Thanks for registering! Please Login...</div>
+
+				<ul>
+					<li>
+						<label for="firstname">firstname : </label>
+						<input class="in" value="'.$names[0].'" type="text" id="firstname" maxlength="64" required="" autofocus="" name="firstname">
+					</li>
+
+					<li>
+						<label for="lastname">lastname : </label>
+						<input class="in" value="'.$names[1].'" type="text" id="lastname" maxlength="64" required="" autofocus="" name="lastname">
+					</li>
+
+					<li>
+						<label for="username">Username : </label>
+						<input class="in" type="text" id="username" maxlength="64" required="" autofocus="" name="username">
+					</li>
+					
+					<li>
+						<label for="password">Password : </label>
+						<input class="in" type="password" id="password" maxlength="128" required="" name="password">
+					</li>
+						
+					<li>
+						<label for="conpassword">Confirm Password : </label>
+						<input class="in" type="password" id="conpassword" maxlength="128" required="" name="conpassword">
+					</li>
+
+					<li>
+						<label for="email">E-mail : </label><br>
+						<input class="in" type="email" id="email" maxlength="128" required="" autofocus="" name="email">
+					</li>
+
+					<li>
+						<label for="school">School : </label><br>
+						<input class="in" type="text" id="school" maxlength="3" required="" autofocus="" name="school">
+					</li>
+
+					<li class="buttonss">
+						<input id="submit" type="button" value="Register">
+					</li>
+				</ul>
+			</form>
+		</div>
+	</body>
+</html>';
+
     }else{
       // to get the login access
       echo "<a href='" . $helper->getLoginUrl(array('email', 'user_about_me')) . "'>Login With FaceBook</a>";
