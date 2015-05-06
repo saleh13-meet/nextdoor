ea24e60d2617b449b77d43df8c57ee14d2d57a26
saleh mansour <saleh.m1998@hotmail.com>
2015-05-06 14:18:35 +0200

    delete acc fix

--- del.php
+++ del.php
@@ -2,18 +2,22 @@
 
 include 'functions.php';
 
+session_start();
+
+check_logged_in_and_active();
+
 connect("u839756306_saleh");
 
 $id = $_SESSION['id'];
 
 $sql = "DELETE FROM users WHERE id = '$id' LIMIT 1";
-$sql2 = "DELETE FROM `info` WHERE id = '$id' LIMIT 1";
+$sql2 = "DELETE FROM info WHERE id = '$id' LIMIT 1";
 $sql3 = "DELETE FROM friends WHERE user1 = '$id' OR user2 = '$id'";
 
 $result = mysql_query($sql)or die(mysql_error());
 $result2 = mysql_query($sql2)or die(mysql_error());
 $result3 = mysql_query($sql3)or die(mysql_error());
 
-echo $id;
+header("location:logout.php");
 
 ?>
