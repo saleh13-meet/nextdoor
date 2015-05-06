9c9c981ea9fcfc6c4c122376ad37e1c4b290a6fd
saleh mansour <saleh.m1998@hotmail.com>
2015-05-06 10:16:36 +0200

    delete account

--- del.php
+++ del.php
@@ -8,9 +8,9 @@ connect("u839756306_saleh");
 
 $id = $_POST['id'];
 
-$sql = "DELETE * FROM users WHERE id = '$id' LIMIT 1";
-$sql2 = "DELETE * FROM info WHERE id = '$id' LIMIT 1";
-$sql3 = "DELETE * FROM friends WHERE user1 = '$id' OR user2 = '$id'";
+$sql = "DELETE FROM users WHERE id = '$id' LIMIT 1";
+$sql2 = "DELETE FROM info WHERE id = '$id' LIMIT 1";
+$sql3 = "DELETE FROM friends WHERE user1 = '$id' OR user2 = '$id'";
 $result = mysql_query($sql);
 $result2 = mysql_query($sql2);
 $result3 = mysql_query($sql3);
