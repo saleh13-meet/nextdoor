95cd44505eeba35f7e961e39dd67d0a5a4f20d29
saleh mansour <saleh.m1998@hotmail.com>
2015-05-06 10:18:07 +0200

    delete account

--- del.php
+++ del.php
@@ -11,9 +11,10 @@ $id = $_POST['id'];
 $sql = "DELETE FROM users WHERE id = '$id' LIMIT 1";
 $sql2 = "DELETE FROM info WHERE id = '$id' LIMIT 1";
 $sql3 = "DELETE FROM friends WHERE user1 = '$id' OR user2 = '$id'";
-$result = mysql_query($sql);
-$result2 = mysql_query($sql2);
-$result3 = mysql_query($sql3);
+
+$result = mysql_query($sql)or die(mysql_error());
+$result2 = mysql_query($sql2)or die(mysql_error());
+$result3 = mysql_query($sql3)or die(mysql_error());
 
 header("location:logout.php");
 
