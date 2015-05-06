a014d37fc5616edcdf443a11416a43cce97d982c
saleh mansour <saleh.m1998@hotmail.com>
2015-05-06 14:04:44 +0200

    delete acc fix

--- del.php
+++ del.php
@@ -9,7 +9,7 @@ connect("u839756306_saleh");
 $id = $_SESSION['id'];
 
 $sql = "DELETE FROM users WHERE id = '$id' LIMIT 1";
-$sql2 = "DELETE FROM info WHERE id = '$id' LIMIT 1";
+$sql2 = "DELETE FROM `info` WHERE `id` = '$id' LIMIT 1";
 $sql3 = "DELETE FROM friends WHERE user1 = '$id' OR user2 = '$id'";
 
 $result = mysql_query($sql)or die(mysql_error());
